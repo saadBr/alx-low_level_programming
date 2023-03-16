ba567affcc7e321265e75fa81e78d7c84e7713c6
saadbr <saadelmabrouk18@gmail.com>
2023-03-16 04:23:32 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error."'
+ * Description: 'prints to the standard error'
  * Return: 1 (Success)
  */
-int main (void)
+int main(void)
 {
 	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
