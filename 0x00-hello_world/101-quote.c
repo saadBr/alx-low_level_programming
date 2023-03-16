b6a2cfe67937d5f8b6f902fd104c87944b9921f6
saadbr <saadelmabrouk18@gmail.com>
2023-03-16 04:20:43 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error."'
+ * Return: 1 (Success)
+ */
+int main (void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
