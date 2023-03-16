45296f92e792d0f485acea42a79f75ef5e842b3b
saadbr <saadelmabrouk18@gmail.com>
2023-03-16 04:39:22 -0700

    test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	char str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf(stderr, "%s", str);
+	fprintf(2, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
