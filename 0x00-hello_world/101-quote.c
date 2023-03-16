bc6dda01e6f22378cde6221703b41c36f17a64b4
saadbr <saadelmabrouk18@gmail.com>
2023-03-16 05:04:40 -0700

    prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,9 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, 
+			"%s",
+		       	"and that piece of art is useful\"
+		       	- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
