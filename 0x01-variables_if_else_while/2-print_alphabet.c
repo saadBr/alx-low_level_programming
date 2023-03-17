ed89c4f35a12d4479c61a2cb895d3f6321f65469
saadbr <saadelmabrouk18@gmail.com>
2023-03-17 03:43:02 -0700

    printing alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description:'print the alphabet.'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	/* your code goes there */
+	for (c = 'a'; c <= 'z'; c++)
+	       putchar(c);
+	putchar('\n');	
+	return (0);
+}
+
