95e87be73740aa56cfc9b503b8310a9f2bd1fa19
saadbr <saadelmabrouk18@gmail.com>
2023-03-17 09:03:18 -0700

    printing all the combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description:'printing all possible different combinations of two digits. '
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9 ; i++)
+	{
+		for (j = i+1 ; j < 10 ; j++)
+		{
+			putchar(48 + i);
+			putchar(48 + j);
+			if (i != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	
+		}
+	}
+	putchar('\n');
+	return (0);
+}
