f7bda72d83dfe1f5313c565a81f0aa2da7f8469d
saadbr <saadelmabrouk18@gmail.com>
2023-03-21 11:21:23 -0700

    printing the sum of numbers that are multiples of 3 or 5 - fixing probs

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,9 +13,9 @@ int main(void)
 
 	for (i = 0; i < 1024 ; i++)
 	{
-		if (( i % 3 == 0 ) || (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 			sum = sum + i;
 	}
-	printf("%d", sum);
+	printf("%d \n", sum);
 	return (0);
 }
