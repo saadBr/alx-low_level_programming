f2c4cc711c0c405f79b98f76c5efb84ce04e9199
saadbr <saadelmabrouk18@gmail.com>
2023-03-21 11:12:53 -0700

    printing the sum of numbers that are multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description:'printing the sum of numbers that are multiple of 3 or 5 '
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1024 ; i++)
+	{
+		if (( i % 3 == 0 ) || (i % 5 == 0))
+			sum =+ i;
+	}
+	printf("%d", sum);
+	return (0);
+}
