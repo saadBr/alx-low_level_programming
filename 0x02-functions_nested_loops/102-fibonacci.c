6d42eea37b33830eab7f3b76ce8d3377da02fb66
saadbr <saadelmabrouk18@gmail.com>
2023-03-21 15:58:41 -0700

    fibonacci of first 98 number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,8 +10,7 @@
 int main(void)
 {
 	long int a = 1, b = 2, sum;
-	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
-	int i, overflow;
+	int i;
 
 	printf("1, 2");
 	for (i = 1; i < 49 ; i++)
@@ -21,28 +20,6 @@ int main(void)
 		b = sum;
 		printf(", %ld", sum);
 	}
-
-	a_head = a / 1000000000;
-	a_tail = a % 1000000000;
-	b_head = b / 1000000000;
-	b_tail = b % 1000000000;
-
-	for (; i < 99; counter++)
-	{
-		overflow = (a_tail + b_tail) / 1000000000;
-		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
-		sum_head = (a_head + b_head) + overflow;
-
-		printf(", %lu%lu", sum_head, sum_tail);
-
-		a_head = b_head;
-		a_tail = b_tail;
-		b_head = sum_head;
-		b_tail = sum_tail;
-	}
-
-
-
 	printf("\n");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description:'printing the first 98 fibonacci number '
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int a = 1, b = 2, sum;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
+	int i, overflow;
+
+	printf("1, 2");
+	for (i = 1; i < 49 ; i++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %ld", sum);
+	}
+
+	a_head = a / 1000000000;
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; i < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
+
+	printf("\n");
+	return (0);
+}
