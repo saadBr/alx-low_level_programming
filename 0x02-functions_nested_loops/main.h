f9cd11bc14429b67fb0efb071fb20176a4d8dca4
saadbr <saadelmabrouk18@gmail.com>
2023-03-21 04:24:07 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+Executable File  104 lines (75 sloc)  1.6 KB
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
