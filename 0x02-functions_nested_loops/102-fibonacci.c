fadb0a8459c086f9a89039dafc6a59ceb39c9a74
saadbr <saadelmabrouk18@gmail.com>
2023-03-21 11:33:31 -0700

    printing first 100 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description:'printing the first 100 fibonacci number '
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 1, b = 2, sum;
+	
+	printf("1 ,2");
+	for (i = 1; i < 100 ; i++)i
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(" ,%d", sum);
+	}
+	return (0);
+}
