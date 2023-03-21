8474c644a4448ba827564773ebcd55a533d76b79
saadbr <saadelmabrouk18@gmail.com>
2023-03-21 09:46:27 -0700

    printing 9 tables with the appropriate style

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,26 +12,22 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if ((i * j) < 10)
+			if (j == 0) 
+				_putchar('0');
+			else if ((i * j) <= 9)
 			{
+				_putchar(',');
+                        	_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + (i * j));
-				if (j < 9)
-				{
-					_putchar(',');
-                        		_putchar(' ');
-					_putchar(' ');
-				}
 			}
 
 			else
 			{
+				_putchar(',');
+                        	_putchar(' ');
 				_putchar('0' + ((i * j) / 10));
-				_putchar('0' + ((i * j) % 10));
-				if (j < 9)
-				{
-					_putchar(',');
-                        		_putchar(' ');
-				}
+                                _putchar('0' + ((i * j) % 10));
 			}
 		}
 		_putchar('\n');
