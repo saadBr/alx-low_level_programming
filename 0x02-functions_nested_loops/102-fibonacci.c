98ee5ce5cf032a55e3f544b0c046bd51850d6caf
saadbr <saadelmabrouk18@gmail.com>
2023-03-21 15:55:18 -0700

    fibonacci of first 98 number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,13 +4,14 @@
 /* betty style doc for function main goes there */
 /**
  * main - Entry point
- * Description:'printing the first 100 fibonacci number '
+ * Description:'printing the first 98 fibonacci number '
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	long int a = 1, b = 2, sum;
-	int i;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
+	int i, overflow;
 
 	printf("1, 2");
 	for (i = 1; i < 49 ; i++)
@@ -20,6 +21,28 @@ int main(void)
 		b = sum;
 		printf(", %ld", sum);
 	}
+
+	a_head = a / 1000000000;
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; i < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
+
 	printf("\n");
 	return (0);
 }
