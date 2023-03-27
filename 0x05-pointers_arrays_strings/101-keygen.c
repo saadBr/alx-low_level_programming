f484b1e81f3cd8f803387c1aa343d9816bb21f28
saadbr <saadelmabrouk18@gmail.com>
2023-03-27 08:21:01 -0700

    generating passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,12 +3,16 @@
 #include <time.h>
 int main(void)
 {
-	int myrand , i;
-
-	for (i =0; i < 10 ; i++)
+	int myrand;
+	int count;
+	int total;
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
 	{
 		myrand = (rand() % 125) + 1;
 		printf("%c", myrand);
+		total -= myrand;
 	}
+	printf("%c", total);
 	return (0);
 }
