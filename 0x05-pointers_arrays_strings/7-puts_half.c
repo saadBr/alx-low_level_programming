fe1bd4309f053f89b9dcbb6d3c002f39a4c3fa21
saadbr <saadelmabrouk18@gmail.com>
2023-03-27 03:20:32 -0700

    printing half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int c = 0, i;
+
+	while (str[c] != '\0')
+		c++;
+	i = c / 2;
+	for (; i < c; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+}
