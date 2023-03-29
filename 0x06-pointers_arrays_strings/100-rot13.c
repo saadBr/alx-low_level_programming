c15b688b7c6e5dc82d8c8b7681c67d6ea7e46283
saadbr <saadelmabrouk18@gmail.com>
2023-03-29 15:37:28 -0700

    coding with rot103

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,7 +14,7 @@ char *rot13(char *s)
 	{
 		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 		{
-			if ((s[i] >= 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
+			if ((s[i] > 'm' && s[i] <= 'z') || (s[i] > 'M' && s[i] <= 'Z'))
 			{
 				s[i] -= 13;
 				break;
