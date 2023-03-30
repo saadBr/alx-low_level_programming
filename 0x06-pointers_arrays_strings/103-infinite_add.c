f4b896db14adba9811d794e64188a990b73ef606
saadbr <saadelmabrouk18@gmail.com>
2023-03-30 03:50:33 -0700

    adding infinite numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -20,25 +20,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	while (*(n2 + j) != '\0')
 	{
-                len2++;
+		len2++;
 		j++;
 	}
 	if (len2 >= len1 && size_r > len2 + 1)
 	{
-		j = len2 - 1;
-		k = len2;
-		for (i = len1 - 1; i >= 0; i--)
+		for (i = len1 - 1, j = len2 - 1, k = len2; i >= 0; i--, j--, k--)
 		{
 			r[k] = (((n1[i] - 48) + (n2[j] - 48) + reste) % 10) + '0';
 			reste = ((n1[i] - 48) + (n2[j] - 48) + reste) / 10;
-			j--;
-			k--;
 		}
-		for (; j>=0 ; j--)
+		for (; j >= 0 ; j--, k--)
 		{
 			r[k] = (((n2[j] - 48) + reste) % 10) + '0';
 			reste = ((n2[j] - 48) + reste) / 10;
-			k--;
 		}
 		if (reste > 0)
 			r[k] = reste + '0';
@@ -55,39 +50,33 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		}
 		return (r);
 	}
-
 	else if (len1 > len2 && size_r > len1 + 1)
-        {
-                j = len1 - 1;
-                k = len1;
-                for (i = len2 - 1; i >= 0; i--)
-                {
-                        r[k] = (((n1[j] - 48) + (n2[i] - 48) + reste) % 10) + '0';
-                        reste = ((n1[j] - 48) + (n2[i] - 48) + reste) / 10;
-                        j--;
-                        k--;
-                }
-                for (; j>=0 ; j--)
-                {
-                        r[k] = (((n1[j] - 48) + reste) % 10) + '0';
-                        reste = ((n1[j] - 48) + reste) / 10;
-                        k--;
-                }
-                if (reste > 0)
-                        r[k] = reste + '0';
-                else
-                {
-                        k = 0;
-                        for (; k < len1; k++)
-                        {
-                                tmp = r[k];
-                                r[k] = r[k + 1];
-                                r[k + 1] = tmp;
-                        }
-                        r[len1] = '\0';
-                }
+	{
+		for (i = len2 - 1, j = len1 - 1, k = len1; i >= 0; i--, j--, k--)
+		{
+			r[k] = (((n1[j] - 48) + (n2[i] - 48) + reste) % 10) + '0';
+			reste = ((n1[j] - 48) + (n2[i] - 48) + reste) / 10;
+		}
+		for (; j >= 0 ; j--, k--)
+		{
+			r[k] = (((n1[j] - 48) + reste) % 10) + '0';
+			reste = ((n1[j] - 48) + reste) / 10;
+		}
+		if (reste > 0)
+			r[k] = reste + '0';
+		else
+		{
+			k = 0;
+			for (; k < len1; k++)
+			{
+				tmp = r[k];
+				r[k] = r[k + 1];
+				r[k + 1] = tmp;
+			}
+			r[len1] = '\0';
+		}
 		return (r);
-        }
+	}
 	else
 		return (0);
 }
