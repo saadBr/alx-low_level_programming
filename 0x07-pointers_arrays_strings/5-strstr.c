23dc1992295166a4f60d409d69378b3bb85950ca
saadbr <saadelmabrouk18@gmail.com>
2023-04-03 09:29:37 -0700

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - find the first occurances string
+ * @haystack: string where the search is made
+ * @needle: string whose occurnce si searched in haystack
+ * Return: return a pointer to nul
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (*haystack)
+	{
+		while (*(haystack + i) == *(needle + i))
+		{
+			if (*(needle + (i + 1)) == '\0')
+				return (haystack);
+			i++;
+		}
+		haystack++;
+	}
+	return (NULL);
+}
