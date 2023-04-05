fccf9f73f956dcadec5629af37ace134a8709231
saadbr <saadelmabrouk18@gmail.com>
2023-04-05 07:23:50 -0700

    is it prime ? - betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
-int is_prime(int n , int i);
+int is_prime(int n, int i);
 /**
  *is_prime_number - check if a number is prime
- *
- * @n: number we check
- * Return : 1 if is prime 0 if it's not.
+ *Description: check for prime numbers
+ *@n: number we check
+ *Return: 1 if is prime 0 if it's not.
  */
 
 int is_prime_number(int n)
@@ -14,17 +14,17 @@ int is_prime_number(int n)
 
 /**
  *is_prime - check if a number is prime
- *
- * @n: number we check
- * @i: div
- * Return : 1 if is prime 0 if it's not.
+ *Description : check for prime numbers
+ *@n: number we check
+ *@i: div
+ *Return: 1 if is prime 0 if it's not.
  */
 
 int is_prime(int n, int i)
 {
 	if (n == i)
 		return (1);
-        if ((n % i == 0 && n < i) || n < 2) 
+	if ((n % i == 0 && n < i) || n < 2)
 		return (0);
-	return (is_prime(n , i +1));
+	return (is_prime(n, i + 1));
 }
