d9dbc3f3968527fc0b8e964a02665117b3ee725e
saadbr <saadelmabrouk18@gmail.com>
2023-04-11 04:50:18 -0700

    creating an array - add desc to return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,10 +3,10 @@
 
 /**
  * create_array - create an array and initilize it with a char
- * @size : size of array
- * @c : char to initialize the array
+ * @size:  size of array
+ * @c: char to initialize the array
  *
- * Return : NULL or pointer to the array
+ * Return: NULL or pointer to the array
  */
 
 char *create_array(unsigned int size, char c)
