2409aa2cae5d9c605bbbb768ecee163d5153bcf6
saadbr <saadelmabrouk18@gmail.com>
2023-04-11 08:29:13 -0700

    alloc two dim arrays - fixing free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * height);
+	arr = malloc(sizeof(int *) * height);
 
 	if (arr == NULL)
 		return (NULL);
@@ -25,6 +25,13 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for(; i>=0; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
