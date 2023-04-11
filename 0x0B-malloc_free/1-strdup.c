5a84d28d09fcf520c46b6e9f3992b22d3af62856
saadbr <saadelmabrouk18@gmail.com>
2023-04-11 05:40:18 -0700

    strdup - malloc corrupted size

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 		return (NULL);
 	for (len = 0; str[len] != '\0'; len++)
 		;
-	newstr = malloc(sizeof(char) * len);
+	newstr = malloc(sizeof(char) * (len + 1));
 	if (newstr == NULL)
 		return (NULL);
 	while (str[i] != '\0')
