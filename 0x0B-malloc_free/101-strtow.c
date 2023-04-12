0791fc77bb100018e514fcbe94b7cb75305de450
saadbr <saadelmabrouk18@gmail.com>
2023-04-11 17:48:45 -0700

    str to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,92 +1,77 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	for (; *s != '\0'; s++)
-		length++;
-	return (length);
-}
-
-/**
- * count - counting the number of word in a string
+ * count_word - counting how many words in a string
  * @s: string to count
  *
  * Return: number of words
  */
 int count(char *s)
 {
-	int i, words, space;
+	int sp, c, words;
 
-	space = 0;
+	sp = 0;
 	words = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[i] == ' ')
-			space = 1;
-		else if (space == 1)
+		if (s[c] == ' ')
+			sp = 0;
+		else if (sp == 0)
 		{
-			space = 0;
+			sp = 1;
 			words++;
 		}
 	}
 
 	return (words);
 }
-
 /**
  * **strtow - splits a string into words
  * @str: string to split
  *
  * Return: pointer to an array of strings or NULL
  */
-
 char **strtow(char *str)
 {
-	int len, words, i, j = 0, counter, end, start;
 	char **arr, *aux;
+	int i, j = 0, len = 0, words, c = 0, start, end;
 
-	len = _strlen(str);
+	while (*(str + len))
+		len++;
 	words = count(str);
 	if (words == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(char *) * (words + 1));
+	arr = (char **) malloc(sizeof(char *) * (words + 1));
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < len ; i++)
+
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
-			if (counter)
+			if (c)
 			{
 				end = i;
-				aux = malloc(sizeof(char) * (counter + 1));
+				aux = (char *) malloc(sizeof(char) * (c + 1));
 				if (aux == NULL)
 					return (NULL);
 				while (start < end)
-				{
 					*aux++ = str[start++];
-				}
 				*aux = '\0';
-				arr[j] = aux - counter;
+				arr[j] = aux - c;
 				j++;
-				counter = 0;
+				c = 0;
 			}
 		}
-		else if (counter++ == 0)
+		else if (c++ == 0)
 			start = i;
 	}
+
 	arr[j] = NULL;
+
 	return (arr);
 }
+
