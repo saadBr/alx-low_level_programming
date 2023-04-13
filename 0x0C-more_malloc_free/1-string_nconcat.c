1967448500435f157f037994fc2e16b478e9f128
saadbr <saadelmabrouk18@gmail.com>
2023-04-13 05:33:51 -0700

    concatenate n char

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
 	{
-		if (j == len2)
+		if (j == len2 - 1)
 			break;
 		str[i] = s2[j];
 	}
