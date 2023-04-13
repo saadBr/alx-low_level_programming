973a06fcddb8259d9f0ffbd5fc7238925653768b
saadbr <saadelmabrouk18@gmail.com>
2023-04-13 10:21:51 -0700

    realloc - fixing stuff

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,29 +15,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr, *temp_ptr;
 	unsigned int i;
-	
+
 	temp_ptr = (char *)ptr;
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size == old_size)
 		return (ptr);
-	else if (new_size > old_size)
-	{
-		new_ptr = malloc(new_size);
-		for (i = 0; i < old_size; i++)
-			new_ptr[i] = temp_ptr[i];
-		free(ptr);
-		return (new_ptr);
-	}
-	else
-	{
-		if (new_size == 0 && ptr != NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		new_ptr = malloc(new_size);
-		for (i = 0; i < new_size; i++)
-			new_ptr[i] = temp_ptr[i];
-		free(ptr);
-		return (new_ptr);
-	}
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	for (i = 0; i < old_size && i < new_size; i++)
+		new_ptr[i] = temp_ptr[i];
+	free(ptr);
+	return (new_ptr);
 }
