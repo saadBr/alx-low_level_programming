ac273b3675519f1d0cfc0d738390ff7f5251f08b
saadbr <saadelmabrouk18@gmail.com>
2023-04-27 11:41:58 -0700

    i am the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * main_parent - executing before main
+ */
+
+void __attribute__((constructor)) main_parent()
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
