4f1e56cd63ab8e288e21666bef11eee2c06a1b33
saadbr <saadelmabrouk18@gmail.com>
2023-04-27 11:48:56 -0700

    assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .text;
+default rel;
+extern printf;
+global main;
+main;
+push rbp;
+mov rdi, format;
+mov rsi, string;
+mov rax, 0;
+call printf wrt ..plt;
+pop rbp;
+mov rax, 0;
+ret;
+section .data;
+string: db "Hello, Holbertson", 10, 0
+format: db "%s", 0
