7c5477937ca4e518453a6f851f99e10244387d3c
saadbr <saadelmabrouk18@gmail.com>
2023-04-27 11:44:03 -0700

    i am the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
 
 void __attribute__((constructor)) main_parent()
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
