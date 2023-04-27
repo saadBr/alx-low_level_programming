6b596ada04f4b1da288f57fdaa50851f598a955f
saadbr <saadelmabrouk18@gmail.com>
2023-04-27 10:40:34 -0700

    weird

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
