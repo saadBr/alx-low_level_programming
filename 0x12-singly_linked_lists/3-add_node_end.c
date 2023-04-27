3c0c0b98b3c80c1b5e8bd2379b767c7f11e94b5e
saadbr <saadelmabrouk18@gmail.com>
2023-04-27 11:21:11 -0700

    add node at end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,7 +17,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next = *head;
+	new->next = NULL;
 	if(!*head)
 	{
 		*head = new;
