12e08ea710e96bf687e2b37e55c6fdb331ec8d57
saadbr <saadelmabrouk18@gmail.com>
2023-05-01 04:26:19 -0700

    checks

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listintint2 - free list
+ * free_listint2 - free list
  *
  * @head: pointer to the first element
  */
@@ -10,10 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	while ((node =  *head))
+	if (head)
 	{
-		*head = (*head)->next;
-		free(node);
+		while ((node = *head))
+		{
+			*head = (*head)->next;
+			free(node);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
