893e50834284e1a478ccbb4c076f2d872d9a4b5a
saadbr <saadelmabrouk18@gmail.com>
2023-05-01 04:15:41 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,14 @@
  * @head: pointer to the first element
  */
 
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	while ((node =  head))
+	while ((node =  *head))
 	{
-		head = head->next;
+		*head = (*head)->next;
 		free(node);
 	}
-	head = NULL;
+	*head = NULL;
 }
