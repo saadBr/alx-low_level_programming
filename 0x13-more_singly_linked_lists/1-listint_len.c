ef5d311e5d7fee85791b7d5c0f6acf99acbd0938
saadbr <saadelmabrouk18@gmail.com>
2023-05-01 03:32:42 -0700

    number of element

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - get list length
+ *
+ * @h: pointer to firts element
+ *
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int c = 0;
+
+	if (h)
+	{
+		while (h->next)
+		{
+			c++;
+			h = h->next;
+		}
+		c++;
+	}
+	return (c);
+}
