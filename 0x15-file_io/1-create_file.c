e1769aee4310706c1b477a9550c0146f720a82d8
saadbr <saadelmabrouk18@gmail.com>
2023-05-08 09:21:48 -0700

    create file - alrighty betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	write_file = write(file, text_content, strlen(text_content));
-	if (write_file == -1 || write_file != (unsigned) strlen(text_content))
+	if (write_file == -1 || write_file != (unsigned int) strlen(text_content))
 	{
 		close(file);
 		return (-1);
